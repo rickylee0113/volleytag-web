69a1942d0d6b29c7b903747c7bd550300471d6b3
rickylee0113 <tpbeian@gmail.com>
2025-12-11 22:08:12 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Vital for Electron to load assets correctly from the file system
+})
